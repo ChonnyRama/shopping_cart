8ea06a7ad043469c36395c1d3ae56a510a85957e
chonny <hitsugiya@gmail.com>
2024-10-11 15:37:33 -0400

    Add logic for adding to cart in CartContext

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -7,11 +7,22 @@ export const CartContextProvider = ({ children }) => {
   const [cart, setCart] = useState([])
 
   const addToCart = (itemId) => {
-    
+    setCart((prevCart) => {
+      const itemExists = prevCart.find(item => item.id === itemId)
+
+      if (itemExists) {
+        return prevCart.map(item => (
+          item.id === itemId
+            ? {...item, amount: item.amount + 1}
+            : item
+        ))
+      }
+      return [...prevCart, {id: itemId, amount: 1}]
+    })
   }
 
   return (
-    <CartContext.Provider value={{}}>
+    <CartContext.Provider value={{cart, addToCart}}>
       {children}
     </CartContext.Provider>
   )
